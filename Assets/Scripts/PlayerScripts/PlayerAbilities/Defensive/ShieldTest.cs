c31030a3255d8589a2a1b532f8602031448fd7fd
nealmcc204 <neal204@hotmail.com>
2016-02-16 21:40:45 +0000

    added shield test ability and made changes to how enemies and abilites work, also changed how enemies and players are stored in managers. general refactoring.

---
+++ Assets/Scripts/PlayerScripts/PlayerAbilities/Defensive/ShieldTest.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShieldTest : DefensiveAbility {
+
+	public override bool Execute(Player target)
+	{
+		target.SetShield (ElementType.FIRE);
+		return true;
+	}
+
+	public override string GetAbilityTag()
+	{
+		return "Shield Test";
+	}
+
+}
