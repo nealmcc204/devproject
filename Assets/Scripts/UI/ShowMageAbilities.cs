42730192b8e95b34ea2b463bc9f15c0ebcd97602
nealmcc204 <neal204@hotmail.com>
2016-03-01 18:37:21 +0000

    adding ui stuff

---
+++ Assets/Scripts/UI/ShowMageAbilities.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShowMageAbilities : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void ShowAbilities()
+	{
+		Debug.Log ("Hello");
+	}
+}
